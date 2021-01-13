124f2a32241236db0f0fe75887205b13497eede8
Amedeo d'Amore <amedeodamore@gmail.com>
2021-01-13 15:05:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 
 def fizzbuzz(int)
   if int % 3 == 0
-    return "Fizz"
+    puts "Fizz"
 elsif int % 5 == 0
-    return "Buzz"
+    puts "Buzz"
 elsif int % 3 == 0 && int % 5 == 0
     return "Fizzbuzz"
 else
